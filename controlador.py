fe168124c52fc431bd0c524db0917e38e8da895a
chulke <gustavogodoy85@gmail.com>
2022-11-01 21:29:44 -0300

    Creacion de clases en modelo y funciones en controlador

--- controlador.py
+++ controlador.py
@@ -5,7 +5,26 @@ from rich.console import Console
 consola = Console()
 
 def cargar_propiedad():
-    pass
+    con = mo.Conectar()
+    consola.print('Ingrese la informacion necesaria para cargar una propiedad.\n')
+    consola.print("Nombre:",style=("bold blue"))
+    nombre = input("-> ")
+    print("\n")
+    consola.print("Dirección:",style=("bold blue"))
+    direccion = input("-> ")
+    print("\n")
+    consola.print("Contacto:",style=("bold blue"))
+    contacto = input("->")
+    print("\n")
+
+    nueva_prop = mo.Propiedad(0, nombre, direccion, contacto )
+    con.cargar_propiedad(nueva_prop)
+    con.conexion.close()
+
+    consola.print(f'La propiedad '{nombre}', ubicada en '{direccion}' fue ingresada con éxito.')
+    input("Presione ENTER para continuar")
+
+
 
 def modif_propiedad():
     pass
--- modelo.py
+++ modelo.py
@@ -13,7 +13,20 @@ class Conectar():
         except mysql.connector.Error as error_ocurrido:
             print('Error en la conexion, error: ', error_ocurrido)
 
-    def cargar_propiedad():
+    def cargar_propiedad(self, propiedad):
+        if self.conexion.is_connected():
+            cursor = self.conexion.cursor()
+            sql = 'INSERT INTO propiedad values(null, %s, %s, %s)'
+            data = (
+                propiedad.getIdProp(),
+                propiedad.getTipo(),
+                propiedad.getEstado(),
+                propiedad.getOperatoria(),
+                propiedad.getPopietario(),
+                propiedad.getNombre(),
+                propiedad.getDireccion(),
+                propiedad.getContacto()
+            )
         pass
 
     def modif_propiedad():
@@ -36,3 +49,130 @@ class Conectar():
 
     def consulta_propiedades_alquiladas():
         pass
+
+class Propiedad():
+    def __init__(self, id_propiedad, tipo, estado, operatoria, propietario, nombre, direccion, contacto):
+        self.id_propiedad = id_propiedad
+        self.tipo = tipo
+        self.estado = estado
+        self.operatoria = operatoria
+        self.propietario = propietario
+        self.nombre = nombre
+        self.direccion = direccion
+        self.contacto = contacto
+
+    def getIdProp(self):
+        return self.id_propiedad
+    def getTipo(self):
+        return self.tipo
+    def getEstado(self):
+        return self.estado
+    def getOperatoria(self):
+        return self.operatoria
+    def getPopietario(self):
+        return self.propietario
+    def getNombre(self):
+        return self.nombre
+    def getDireccion(self):
+        return self.direccion
+    def getContacto(self):
+        return self.contacto
+
+    def setIdProp(self,id_propiedad):
+        self.id_propiedad = id_propiedad
+    def setTipo(self,tipo):
+        self.tipo = tipo
+    def setEstado(self,estado):
+        self.estado = estado
+    def setOperatoria(self,operatoria):
+        self.operatoria = operatoria
+    def setPropietario(self,propietario):
+        self.propietario = propietario
+    def setNombre(self,nombre):
+        self.nombre = nombre
+    def setDireccion(self,direccion):
+        self.direccion = direccion
+    def setContacto(self,contacto):
+        self.contacto = contacto
+
+class Estado():
+    def __init__(self,id_estado, nombre):
+        self.id_estado = id_estado
+        self.nombre = nombre
+
+    def __str__(self) -> str:
+        return str(self.id_estado)+' '+self.nombre
+
+    def getIdEstado(self):
+        return self.id_estado
+    def getNombre(self):
+        return self.nombre
+
+    def setIdEstado(self,id_estado):
+        self.id_estado = id_estado
+    def setNombre(self,nombre):
+        self.nombre = nombre
+
+class Tipo():
+    def __init__(self,id_tipo, nombre):
+        self.id_tipo = id_tipo
+        self.nombre = nombre
+
+    def __str__(self) -> str:
+        return str(self.id_tipo)+' '+self.nombre
+
+    def getIdTipo(self):
+        return self.id_tipo
+    def getNombre(self):
+        return self.nombre
+
+    def setIdTipo(self,id_tipo):
+        self.id_tipo = id_tipo
+    def setNombre(self,nombre):
+        self.nombre = nombre
+
+class Operatoria():
+    def __init__(self,id_op, nombre):
+        self.id_operatoria = id_op
+        self.nombre = nombre
+
+    def __str__(self) -> str:
+        return str(self.id_operatoria)+' '+self.nombre
+
+    def getIdOperatoria(self):
+        return self.id_operatoria
+    def getNombre(self):
+        return self.nombre
+
+    def setIdOperatoria(self,id_op):
+        self.id_operatoria = id_op
+    def setNombre(self,nombre):
+        self.nombre = nombre
+
+class Propietario():
+    def __init__(self,id_propietario, nombre, direccion, contacto):
+        self.id_propietario = id_propietario
+        self.nombre = nombre
+        self.direccion = direccion
+        self.contacto = contacto
+
+    def __str__(self) -> str:
+        return str(self.id_propietario)+' '+self.nombre
+
+    def getIdTipo(self):
+        return self.id_propietario
+    def getNombre(self):
+        return self.nombre
+    def getDireccion(self):
+        return self.direccion
+    def getContacto(self):
+        return self.contacto
+
+    def setIdTipo(self,id_propietario):
+        self.id_propietario = id_propietario
+    def setNombre(self,nombre):
+        self.nombre = nombre
+    def setDireccion(self, direccion):
+        self.direccion = direccion
+    def setContacto(self, contacto):
+        self.contacto = contacto
